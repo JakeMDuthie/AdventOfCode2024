c4736c4aab7566c32a21a971e958a3947f70abec
Jake Duthie <jakeduthie@rocketmail.com>
2024-12-02 17:00:00 +0000

    Data files

--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace AdventOfCode
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode
 {
-    internal class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
+            var sr = new StreamReader(
+                Path.Combine(Environment.CurrentDirectory, "..\\..\\Data\\historianHysteriaSmall.txt"));
+
+            Console.WriteLine("Hello World!");
+            var line = sr.ReadLine();
+            
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                
+                line = sr.ReadLine();
+            }
         }
     }
 }
