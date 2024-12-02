b329ece5779e189ab0f41c4cfbcc744546ca4531
Jake Duthie <jakeduthie@rocketmail.com>
2024-12-02 18:25:49 +0000

    Tidied up program

--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -6,16 +6,18 @@ namespace AdventOfCode
     {
         public static void Main(string[] args)
         {
-            var reactorAnalyser = new ReactorAnalyser("..\\..\\Data\\redNosedReactor.txt");
+            var reactorAnalyser = new ReactorAnalyser(@"..\..\Data\redNosedReactor.txt");
             
+            Console.WriteLine("Day 2: Red-Nosed Reports");
             Console.WriteLine($"SOLUTION pt 1 = {reactorAnalyser.GetSafeReports()}");
             Console.WriteLine($"SOLUTION pt 2 = {reactorAnalyser.GetSafeReportsWithinTolerance()}");
         }
 
         private static void Day1()
         {
-            var idAnalyser = new IdAnalyser("..\\..\\Data\\historianHysteria.txt");
-            Console.WriteLine($"SOLUTION = {idAnalyser.GetTotalDifferenceBetweenLists()}");
+            Console.WriteLine("Day 1: Historian Hysteria");
+            var idAnalyser = new IdAnalyser(@"..\..\Data\historianHysteria.txt");
+            Console.WriteLine($"SOLUTION pt 1 = {idAnalyser.GetTotalDifferenceBetweenLists()}");
             Console.WriteLine($"SOLUTION pt 2 = {idAnalyser.GetSimilarityScoreTotal()}");
         }
     }
